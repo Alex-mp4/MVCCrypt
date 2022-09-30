4cc55d4dfaffa0b36399ecdf4515d3ce3006362b
Alex-mp4 <alexander.heino@elev.ga.ntig.se>
2022-09-30 13:42:26 +0200

    Binary converter attempt

--- src/ModelCrypt.java
+++ src/ModelCrypt.java
@@ -5,20 +5,31 @@ class CompressModel {
 
     public static void main(String[] args) {
         String message = JOptionPane.showInputDialog("Message"); //take message from view-textfield
+        //String IMess = Integer.toBinaryString(Integer.valueOf(Integer.parseInt(message)));
+        String IMess = "";
+        int key = '(';
+        String IKey = Integer.toBinaryString(key);
+        for (int o = 1; o < message.length(); o++) {
+            char c = message.charAt(o);
+            System.out.println((int)c);
+            IMess += Integer.toBinaryString(c);
+            IKey = IKey + IKey;
+        }
+        System.out.print(IMess);
         String cryptOrDecrypt = JOptionPane.showInputDialog("Crypt or decrypt"); //radiobutton choice
         String cmess = "";
-        int key = '(';
         int x = 0;
         char v = 0;
-        for (int i = 0; i < message.length(); i++) {
-            int c = message.charAt(x);
+        for (int i = 0; i < IMess.length(); i++) {
+            int c = IMess.charAt(x);
+            int k = IKey.charAt(x);
             if (cryptOrDecrypt == "crypt") {
-                v = (char)scrobbler(c, key);
+                v = (char)scrobbler(c, k);
             }
             else {
-                v = (char)descrobbler(key, c);
+                v = (char)descrobbler(k, c);
             }
-            cmess = cmess + v;
+            cmess = cmess + v; //convert v (should be in binary) to decimal, then to ascii
             x++;
             if (x > message.length()) {
                 x = 0;
@@ -28,12 +39,12 @@ class CompressModel {
         //messageValue = cmess;
     }
 
-    private static int descrobbler(int key, int c) {
-        return (c^key);
+    private static int descrobbler(int k, int c) {
+        return (c^k);
     }
 
-    private static int scrobbler(int c, int key) {
-        return (c^key);
+    private static int scrobbler(int c, int k) {
+        return (c^k);
     }
 
     public String getMessageValue() {
--- src/ViewCrypt.java
+++ src/ViewCrypt.java
@@ -5,8 +5,8 @@ public class ViewCrypt {
     private JTextField textField1 = new JTextField("Input");
     private JTextArea textArea1 = new JTextArea("Output");
     private JButton button1 = new JButton("");
-    private JRadioButton radioButtonCrypt = new JRadioButton("Crypt");
-    private JRadioButton radioButtonDeCrypt = new JRadioButton("Decrypt");
+    private JRadioButton radioButton1 = new JRadioButton("Crypt");
+    private JRadioButton radioButton2 = new JRadioButton("Decrypt");
 
     //JRadioButton cryptButton = radioButtonCrypt;
     //JRadioButton decryptButton = radioButtonDeCrypt;
@@ -17,13 +17,14 @@ public class ViewCrypt {
 
     ViewCrypt() {
         JPanel panel = new JPanel();
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(600, 200);
+        //this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //this.setSize(600, 200);
         panel.add(textField1);
         panel.add(textArea1);
-        panel.add(radioButtonCrypt);
-        panel.add(radioButtonDeCrypt);
-        this.add(panel);
+        panel.add(button1);
+        panel.add(radioButton1);
+        panel.add(radioButton2);
+        //this.add(panel);
     }
 
     public String getTextField() {
